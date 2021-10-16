792e095e87a25a73d2cc3df37d94b9de3dec4f86
John Teckemeyer <johnteckemeyer@gmail.com>
2021-10-16 00:30:02 -0700

    Create smiley face button component to start new game

--- src/Components/Controls/Controls.jsx
+++ src/Components/Controls/Controls.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import FlagCounter from '../FlagCounter/FlagCounter';
 import GameSelect from '../GameSelect/GameSelect';
+import NewGameButton from '../NewGameButton/NewGameButton';
 import './Controls.scss';
 
 export default function Controls (props) {
@@ -10,6 +11,7 @@ export default function Controls (props) {
             <div id="controls-cluster">
                 <FlagCounter count={"04"} />
                 <GameSelect level={props.level} updateLevel={props.updateLevel} />
+                <NewGameButton />
             </div>
         </div>
     )
---
+++ src/Components/NewGameButton/NewGameButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './NewGameButton.scss';
+
+export default function NewGameButton (props) {
+    return (
+        <div id="newgamebutton-container">
+            <div className="material-icons icon-medium">emoji_emotions</div>
+        </div>
+    )
+}
