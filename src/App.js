86e6a0ddd2a7a96f7cfcd791ddbbd4aa245ec394
John Teckemeyer <johnteckemeyer@gmail.com>
2021-10-17 20:58:02 -0700

    Add object containing the data for the different difficulty levels

--- src/App.js
+++ src/App.js
@@ -5,7 +5,27 @@ import './App.scss';
 
 function App() {
 
+  // level will have the values: 'easy', 'medium' or 'hard'
   const [level, setLevel] = useState('easy')
+  
+  // Object holding the options for board sizes and bomb count
+  const boardData = {
+    easy: {
+        x: 10,
+        y: 10,
+        bombs: 10
+    },
+    medium: {
+        x: 16,
+        y: 16,
+        bombs: 40
+    },
+    hard: {
+        x: 30,
+        y: 16,
+        bombs: 99
+    }
+  }
 
   function updateLevel (newLevel) {
     setLevel(newLevel);
