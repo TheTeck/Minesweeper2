ac0eb474db82a2bdc1baceb749ac140e6f991574
John Teckemeyer <johnteckemeyer@gmail.com>
2021-10-25 21:24:22 -0700

    Add logic in Cell component to toggle flags with a right click. And lift state to app file to update the controls and flag counter components

--- src/Components/Cell/Cell.jsx
+++ src/Components/Cell/Cell.jsx
@@ -8,6 +8,11 @@ export default function Cell (props) {
         props.handleCellClick(props.cell.x, props.cell.y);
     }
 
+    function handleRightClick (e) {
+        e.preventDefault();
+        props.toggleFlag(props.cell.x, props.cell.y);
+    }
+
     return (
         <div 
             className="cell-container"
@@ -17,9 +22,10 @@ export default function Cell (props) {
             }}
         >
             {
-                !props.cell.exposed ? <div onClick={handleLeftClick} className="unexposed-cell" style={{ backgroundColor: 'maroon' }}></div>
+                !props.cell.exposed ? (props.cell.flagged ? <div onClick={handleLeftClick} onContextMenu={handleRightClick} className="unexposed-cell material-icons icon-tiny" style={{ backgroundColor: 'maroon' }}>flag</div>
+                : <div onClick={handleLeftClick} onContextMenu={handleRightClick} className="unexposed-cell" style={{ backgroundColor: 'maroon' }}></div>)
                 : props.cell.value === 9 ? <div className="material-icons icon-tiny">wb_sunny</div>
-                :<div className={`cell${props.cell.value}`}> {props.cell.value ? props.cell.value : ''}</div>
+                : <div className={`cell${props.cell.value}`}> {props.cell.value ? props.cell.value : ''}</div>
             }
         </div>
     )
--- src/Components/Controls/Controls.jsx
+++ src/Components/Controls/Controls.jsx
@@ -10,7 +10,7 @@ export default function Controls (props) {
     return (
         <div id="controls-container">
             <div id="controls-cluster">
-                <FlagCounter count={"04"} />
+                <FlagCounter count={props.flags} />
                 <GameSelect level={props.level} updateLevel={props.updateLevel} />
                 <NewGameButton />
                 {/* False for now. should change when game starts */}
